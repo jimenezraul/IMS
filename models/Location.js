0d1efcc9dbd7750125526a050fd361209448b7c6
Raul <jimenezraul1981@gmail.com>
2022-03-30 10:04:30 -0400

    added category and location model

---
+++ models/Location.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Location extends Model { }
+
+Location.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        slot: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: false,
+            validate: {
+                len: [1]
+            }
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: false,
+            validate: {
+                len: [1]
+            }
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "product",
+                key: "id"
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: false,
+        modelName: 'location'
+    }
+);
+
+module.exports = Location;
