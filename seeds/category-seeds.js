a0f46be5e3d29996ad1878415784911cd88c9511
Raul <jimenezraul1981@gmail.com>
2022-04-01 08:20:27 -0400

    added categories seeds

---
+++ seeds/category-seeds.js
@@ -0,0 +1,17 @@
+const { Category } = require("../models");
+
+const categoryData = [
+    {
+        name: "Foodservice Supplies"
+    },
+    {
+        name: "Cleaning Supplies"
+    },
+    {
+        name: "Paper Goods"
+    },
+];
+
+const seedCategories = () => Category.bulkCreate(categoryData);
+
+module.exports = seedCategories;
