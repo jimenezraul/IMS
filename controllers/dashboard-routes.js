5cba23b80a022e3a4ca4542caf4f3f62140b4e87
Raul <jimenezraul1981@gmail.com>
2022-04-03 18:37:17 -0400

    added filter product by category

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,18 +5,47 @@ const { Category, Product, User, Location, Supplier } = require("../models");
 router.get("/", withAuth, async (req, res) => {
   // get all categories
   const categoryData = await Category.findAll({});
-  const category = categoryData.map((category) =>
+  const categories = categoryData.map((category) =>
     category.get({ plain: true })
   );
+    
   // get all products
   const productData = await Product.findAll({});
   const product = productData.map((product) => product.get({ plain: true }));
 
   res.render("dashboard", {
     loggedIn: req.session.loggedIn,
-    categories: category,
+    categories: categories,
     products: product,
   });
+});
+
+// get product by category id
+router.get("/category/:id", withAuth, async (req, res) => {
+    // get all categories
+    const categoryData = await Category.findAll({});
+    const categories = categoryData.map((category) =>
+        category.get({ plain: true })
+    );
+    //name of category
+    const categoryNameData = await Category.findByPk(req.params.id, {
+        attributes: ["name"],
+    });
+    const category = categoryNameData.get({ plain: true });
+    console.log(category.name);
+    // get product by category id
+    const productData = await Product.findAll({
+        where: {
+            category_id: req.params.id,
+        },
+    }); 
+    const product = productData.map((product) => product.get({ plain: true }));
+    res.render("dashboard", {
+        loggedIn: req.session.loggedIn,
+        categories: categories,
+        products: product,
+        title: `Products by ${category.name}`,
+    });
 });
 
 module.exports = router;
