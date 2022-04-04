0510ee297143e519604a52c6a12593433d71a23c
Raul <jimenezraul1981@gmail.com>
2022-04-04 15:24:28 -0400

    added supplier card info and formatPhone helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,15 @@
 module.exports = {
-    count: (index) => {
-      index += 1;
-      return index;
-    },
-  };
-  
+  count: (index) => {
+    index += 1;
+    return index;
+  },
+  // fotmat phone number
+  formatPhone: (phone) => {
+    const phoneNumber = phone.replace(/\D/g, "");
+    const phoneFormatted = phoneNumber.replace(
+      /(\d{3})(\d{3})(\d{4})/,
+      "($1) $2-$3"
+    );
+    return phoneFormatted;
+  },
+};
