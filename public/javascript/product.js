967f38a544b63ccb4b7d1985ade5e0d11649efb4
Raul <jimenezraul1981@gmail.com>
2022-04-04 23:18:28 -0400

    add supplier update form

--- public/javascript/product.js
+++ public/javascript/product.js
@@ -6,7 +6,6 @@ async function updateProduct(event) {
   const productId = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
-  console.log("helloooo", productId);
 
   const name = document.querySelector("#product-name").value.trim();
   const description = document
@@ -36,4 +35,45 @@ async function updateProduct(event) {
   }
 }
 
+// handle supplier form 
+const supplierForm = document.querySelector("#update-supplier-form");
+
+async function updateSupplier(event) {
+  event.preventDefault();
+
+  const supplierId = supplierForm.getAttribute("supplier_id");
+
+  const name = document.querySelector("#supplier-name").value.trim();
+  const phone = document.querySelector("#supplier-phone").value.trim();
+  const email = document.querySelector("#supplier-email").value.trim();
+  const address = document.querySelector("#supplier-address").value.trim();
+  const city = document.querySelector("#supplier-city").value.trim();
+  const state = document.querySelector("#supplier-state").value.trim();
+  const zip = document.querySelector("#supplier-zip").value.trim();
+
+  if (name && phone && email && address && city && state && zip) {
+    const response = await fetch(`/api/suppliers/${supplierId}`, {
+      method: "put",
+      body: JSON.stringify({
+        name,
+        phone,
+        email,
+        address,
+        city,
+        state,
+        zip,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  } else {
+    alert("Please, fill out all fields");
+  }
+}
+
 productForm.addEventListener("submit", updateProduct);
+supplierForm.addEventListener("submit", updateSupplier);
